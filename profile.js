2496f0898bc345d61ed23590f1a8a7ed76ac1eab
samirbsihowkarma <B_K-S2@ulster.ac.uk>
2026-08-08 01:34:30 +0100

    on click function

--- profile.js
+++ profile.js
@@ -12,5 +12,15 @@
       '<div class="profile-actions"><a class="btn btn-primary" href="sell.html">List item</a><a class="btn btn-ghost" href="chat.html">Message</a></div>'
     );
 
+    $('.profile-tabs button').on('click', function () {
+      var tab = $(this).data('tab');
+      $('.profile-tabs button').removeClass('active');
+      $(this).addClass('active');
+      $('.profile-panel').attr('hidden', true);
+      $('#' + tab + 'Panel').removeAttr('hidden');
+      if (window.tgObserve) window.tgObserve();
+    });
+
+
   });
 })(jQuery);
